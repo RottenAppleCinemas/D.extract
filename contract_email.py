1042a45bb5f9528979c14110ea542c5fb1a0c9ce
YuWon <162537419+RottenAppleCinemas@users.noreply.github.com>
2025-06-02 11:53:41 -0400

    Create contract_email.py

---
+++ contract_email.py
@@ -0,0 +1,14 @@
+Contract Action-  
+Account Name:  
+Customer Type: 
+Ship To: 
+Ship To Email:  
+Region:  
+Shipping Country:  
+SalesForce Full Account ID:  
+Opportunity:  
+Quote:  
+SKU :  
+Start Date:  
+End Date:  
+Quantity:  
